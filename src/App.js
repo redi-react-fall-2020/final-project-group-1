389433f72cf103041d5d3f7b63b0821e94d6f0f1
Ahmad Al masri <hdedan.sy@gmail.com>
2020-11-23 19:26:13 +0100

    Fetch API
    Fetched data from API and stored it in state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
 
 function App() {
+  const [restaurants, setRestaurants] = useState([])
+  const getData = async () => {
+    const response = await fetch("https://redi-final-restaurants.herokuapp.com/restaurants");
+    const data = await response.json();
+    console.log(data.results);
+    return data.results;
+  };
+  useEffect(() => {
+    getData().then((result) => {
+      setRestaurants(result);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
