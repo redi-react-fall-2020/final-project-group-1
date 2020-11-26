79579e2580e8612cf1d0cf6f0422f7527b390558
halilesmer <taximinator@gmail.com>
2020-11-26 17:01:33 +0100

    add Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./Theme";
+import { GlobalStyles } from "./GlobalDarkLightMode";
+import { Text } from "./Text";
 import './App.css';
+
+
 import { Cards } from './Cards'
 
+
+
 function App() {
+const [theme, setTheme] = useState("Light Mode");
+console.log("theme. " + theme);
+
+// The function that toggles between themes
+const toggleTheme = () => {
+  // if the theme is not light, then set it to dark
+  if (theme === "Light Mode") {
+    setTheme("Dark Mode");
+    // otherwise, it should be light
+  } else {
+    setTheme("Light Mode");
+  }
+};
+
   return (
-    <div className="App">
-      <Cards/>
-    </div>
+    <ThemeProvider theme={theme === "Light Mode" ? darkTheme : lightTheme}>
+
+      <Cards />
+
+      <Text />
+      <button onClick={toggleTheme}>{theme}</button>
+      <GlobalStyles />
+      {/* Pass the toggle functionality to the button */}
+
+
+    </ThemeProvider>
   );
 }
 
---
+++ src/Text.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    <p>
+      Text (lateinisch texere ‚weben', ‚flechten') bezeichnet im
+      nichtwissenschaftlichen Sprachgebrauch eine abgegrenzte, zusammenhängende,
+      meist schriftliche sprachliche Äußerung, im weiteren Sinne auch nicht
+      geschriebene, aber schreibbare Sprachinformation (beispielsweise eines
+      Liedes, Films oder einer improvisierten ...ext (lateinisch texere ‚weben',
+      ‚flechten') bezeichnet im nichtwissenschaftlichen Sprachgebrauch eine
+      abgegrenzte, zusammenhängende, meist schriftliche sprachliche Äußerung, im
+      weiteren Sinne auch nicht geschriebene, aber schreibbare Sprachinformation
+    </p>
+  );
+};
+export { Text };
