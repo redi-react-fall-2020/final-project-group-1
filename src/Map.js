ee5a8fe185b3a3c222decf69bed139ecab5980fe
Ahmad Al masri <hdedan.sy@gmail.com>
2020-12-06 15:29:55 +0100

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -5,7 +5,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 export const  Map = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCd-mpiJAWuTXM_BCCPcoFHubGHMd3Qu8s&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=######&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
